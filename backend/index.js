a562b9af3b330fb81f166ec527c13aa58b637def
Joao Gardin <gardin1992@gmail.com>
2024-08-27 23:37:07 -0300

    add dockerfile in backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const PORT = 3000;
+const PORT = 3005;
 const HOST = "0.0.0.0";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! -_-");
 });
 
 app.listen(PORT, HOST);
